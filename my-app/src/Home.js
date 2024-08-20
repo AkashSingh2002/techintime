ea7d7c7bb02cabab944bc501f1f70b9f0a3f393c
Akash Singh <88923478+AkashSingh2002@users.noreply.github.com>
2024-08-20 11:55:17 +0000

    changes3

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -4,15 +4,20 @@ import ContactUs from './pages/contactus';
 const Home = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slides = [
-    "/ind1.jpg",
-    "/ind2.jpg",
-    "/ind3.jpg",
-    // Add more images as needed
+    { src: "/image.png", description: "valve" },
+    { src: "/image4.png", description: "Advanced Equipment and Tools" },
+    { src: "/cable3.jpg", description: "High-Quality Industrial Products" },
+    { src: "/image5.png", description: "Comprehensive Consultancy Services" },
+    { src: "/img9.jpg", description: "Expert Technical Services" },
+    { src: "/led.jpg", description: "Tailored Solutions for Your Needs" },
+    // Add more images and descriptions as needed
   ];
+
+  const slidesToShow = 2;
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+      setCurrentSlide(prevSlide => (prevSlide + 1) % Math.ceil(slides.length / slidesToShow));
     }, 3000); // Change image every 3 seconds
 
     return () => clearInterval(interval);
@@ -20,36 +25,81 @@ const Home = () => {
 
   return (
     <div className="relative w-full">
+      {/* About Us Section */}
+      <section className="container mx-auto my-12">
+        <h1 className="text-center text-4xl font-bold mb-8">Technical Solutions Delivered in Time</h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="text-center">
+            <img
+              src="https://img1.wsimg.com/isteam/ip/c43c367a-a7e5-4f46-83a0-da85970be876/IMG-20230221-WA0028.jpg/:/cr=t:0%25,l:12.5%25,w:75%25,h:100%25/rs=w:1095,h:1095,cg=true"
+              alt="Technical Products"
+              className="mx-auto rounded-full mb-4 w-48 h-48" // Increased size to 12rem x 12rem
+            />
+            <h2 className="text-xl font-semibold mb-2">Consultancy</h2>
+            <p>
+              At TECHinTIME, we have a pool of expert consultants and trainers for ASNT Level-II certifications,
+              quality management systems, and documentations for ISO certification.
+            </p>
+          </div>
+
+          <div className="text-center">
+            <img
+              src="https://img1.wsimg.com/isteam/ip/c43c367a-a7e5-4f46-83a0-da85970be876/istockphoto-618651576-612x612.jpg/:/cr=t:0%25,l:16.67%25,w:66.67%25,h:100%25/rs=w:365,h:365,cg=true"
+              alt="Technical Services"
+              className="mx-auto rounded-full mb-4 w-48 h-48" // Increased size to 12rem x 12rem
+            />
+            <h2 className="text-xl font-semibold mb-2">Technical Services</h2>
+            <p>
+              At TECHinTIME, through our channel partners, we provide specialized technical services like
+              contractor for mechanical & electrical packages of infra projects, welding and NDT services, and more.
+            </p>
+          </div>
+
+          <div className="text-center">
+            <img
+              src="https://img1.wsimg.com/isteam/ip/c43c367a-a7e5-4f46-83a0-da85970be876/images%20(1).jpg/:/cr=t:0%25,l:18.95%25,w:62.11%25,h:100%25/rs=w:365,h:365,cg=true"
+              alt="Consultancy"
+              className="mx-auto rounded-full mb-4 w-48 h-48" // Increased size to 12rem x 12rem
+            />
+            <h2 className="text-xl font-semibold mb-2">Technical Products</h2>
+            <p>
+              At TECHinTIME, our mission is to be your trusted supplier of high-quality industrial products,
+              equipment, tools, instruments, consumables, and spares to meet all your urgent and critical
+              requirements in time.
+            </p>
+          </div>
+        </div>
+      </section>
+
       {/* Image Carousel */}
       <div className="relative w-full h-[75vh] overflow-hidden">
-        <div
-          className="flex transition-transform duration-1000 ease-in-out"
-          style={{ transform: `translateX(-${currentSlide * 100}%)` }}
-        >
+        {/* Explore Our Products and Services - Top Overlay */}
+        <div className="absolute top-0 left-0 w-full p-4 text-center bg-white bg-opacity-75 z-10">
+          <h1 className="text-3xl font-bold">Explore our Products and Services</h1>
+        </div>
+
+        <div className="flex transition-transform duration-1000 ease-in-out mt-9"
+             style={{ transform: `translateX(-${currentSlide * (100 / slidesToShow)}%)` }}>
           {slides.map((slide, index) => (
-            <div key={index} className="flex-shrink-0 w-full h-full">
-              <img
-                src={slide}
-                alt={`Slide ${index + 1}`}
-                className="w-full h-full object-cover"
-                style={{ maxHeight: '100%' }} // Adjust the max height of the images
-              />
+            <div key={index} className="flex-shrink-0 w-full md:w-1/3 h-full flex flex-col items-center p-4">
+              <div className="relative w-full flex-1 flex items-center justify-center">
+                <img
+                  src={slide.src}
+                  alt={`Slide ${index + 1}`}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </div>
+              <p className="mt-4 text-center text-lg font-semibold bg-black text-white px-4 py-2 rounded w-full">
+                {slide.description}
+              </p>
             </div>
           ))}
-        </div>
-        {/* Overlay content */}
-        <div className="absolute top-0 left-0 w-full p-4 text-center bg-white bg-opacity-75">
-          <h1 className="text-3xl font-bold">Welcome to TECHinTIME</h1>
-          <p className="mt-2 text-lg">Explore our Products and Services.</p>
-        </div>
-        <div className="absolute bottom-0 left-0 w-full p-4 text-center bg-white bg-opacity-75">
-          <h1 className="text-3xl font-bold">Description about the products</h1>
-          <p className="mt-2 text-lg">Explore our Products and Services.</p>
         </div>
       </div>
 
       {/* Contact Us Section */}
-      <div id="contactus" className="p-4 ">
+      <div id="contactus" className="p-4">
         <ContactUs />
       </div>
     </div>
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -9,51 +9,51 @@ const Navbar = () => {
   return (
     <>
       {/* Contact Information with Blue Line */}
-      <div className="bg-blue-600 text-white py-2 text-center text-sm">
+      <div className="bg-blue-600 text-white py-3 text-center text-base">
         Contact : +91 9289199700
       </div>
       <nav className="bg-white shadow-md">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between items-center h-20">
+        <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-12">
+          <div className="flex justify-between items-center h-24">
             <div className="flex items-center">
               {/* Logo */}
               <img 
-                className="h-16 w-auto" 
+                className="h-20 w-auto" 
                 src="https://img1.wsimg.com/isteam/ip/c43c367a-a7e5-4f46-83a0-da85970be876/techin%20time%20logo-6bc657c.jpg/:/rs=h:118,cg=true,m/qt=q:95" 
                 alt="TechinTime Logo" 
               />
             </div>
-            <div className="hidden md:flex space-x-8">
-              <a href="/" className="text-gray-900 inline-flex items-center px-1 pt-1 text-sm font-medium">
+            <div className="hidden md:flex space-x-12">
+              <a href="/" className="text-gray-900 inline-flex items-center px-2 pt-2 text-lg font-medium hover:text-gray-700">
                 Home
               </a>
-              <a href="/about" className="text-gray-900 inline-flex items-center px-1 pt-1 text-sm font-medium">
+              <a href="/about" className="text-gray-900 inline-flex items-center px-2 pt-2 text-lg font-medium hover:text-gray-700">
                 About Us
               </a>
               {/* Products Dropdown */}
               <div className="relative">
                 <button 
                   onClick={() => setIsProductsOpen(!isProductsOpen)} 
-                  className="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  className="text-gray-500 hover:text-gray-700 inline-flex items-center px-2 pt-2 text-lg font-medium"
                 >
                   Products {isProductsOpen ? (
-                    <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20" className="ml-1" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M19 15l-7-7-7 7" />
-                  </svg>
-                ) : (
-                  <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20" className="ml-1" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M19 9l-7 7-7-7" />
-                </svg>
-                )}
+                    <svg viewBox="0 0 24 24" fill="currentColor" width="24" height="24" className="ml-2" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 15l-7-7-7 7" />
+                    </svg>
+                  ) : (
+                    <svg viewBox="0 0 24 24" fill="currentColor" width="24" height="24" className="ml-2" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 9l-7 7-7-7" />
+                    </svg>
+                  )}
                 </button>
                 {isProductsOpen && (
-                  <div className="absolute mt-2 bg-white shadow-lg rounded-md p-2 w-48 z-10">
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Pipes and Fittings</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Fastners & Gaskets</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Industrial Valves</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Electrical Cables, Panels</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Industrial Tools</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Measuring Instruments</p>
+                  <div className="absolute mt-2 bg-white shadow-lg rounded-md p-4 w-64 z-10">
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Pipes and Fittings</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Fastners & Gaskets</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Industrial Valves</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Electrical Cables, Panels</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Industrial Tools</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Measuring Instruments</p>
                   </div>
                 )}
               </div>
@@ -61,22 +61,22 @@ const Navbar = () => {
               <div className="relative">
                 <button 
                   onClick={() => setIsSparesOpen(!isSparesOpen)} 
-                  className="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  className="text-gray-500 hover:text-gray-700 inline-flex items-center px-2 pt-2 text-lg font-medium"
                 >
                   Spares {isSparesOpen ? (
-                    <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20" className="ml-1" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M19 15l-7-7-7 7" />
-                  </svg>
-                ) : (
-                  <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20" className="ml-1" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M19 9l-7 7-7-7" />
-                </svg>
-                )}
+                    <svg viewBox="0 0 24 24" fill="currentColor" width="24" height="24" className="ml-2" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 15l-7-7-7 7" />
+                    </svg>
+                  ) : (
+                    <svg viewBox="0 0 24 24" fill="currentColor" width="24" height="24" className="ml-2" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 9l-7 7-7-7" />
+                    </svg>
+                  )}
                 </button>
                 {isSparesOpen && (
-                  <div className="absolute mt-2 bg-white shadow-lg rounded-md p-2 w-48 z-10">
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Major & OEM Supplied</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Off the Shelf Spares</p>
+                  <div className="absolute mt-2 bg-white shadow-lg rounded-md p-4 w-48 z-10">
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Major & OEM Supplied</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Off the Shelf Spares</p>
                   </div>
                 )}
               </div>
@@ -84,29 +84,29 @@ const Navbar = () => {
               <div className="relative">
                 <button 
                   onClick={() => setIsServicesOpen(!isServicesOpen)} 
-                  className="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  className="text-gray-500 hover:text-gray-700 inline-flex items-center px-2 pt-2 text-lg font-medium"
                 >
                   Services {isServicesOpen ? (
-                    <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20" className="ml-1" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M19 15l-7-7-7 7" />
-                  </svg>
-                ) : (
-                  <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20" className="ml-1" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M19 9l-7 7-7-7" />
-                </svg>
-                )}
+                    <svg viewBox="0 0 24 24" fill="currentColor" width="24" height="24" className="ml-2" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 15l-7-7-7 7" />
+                    </svg>
+                  ) : (
+                    <svg viewBox="0 0 24 24" fill="currentColor" width="24" height="24" className="ml-2" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 9l-7 7-7-7" />
+                    </svg>
+                  )}
                 </button>
                 {isServicesOpen && (
-                  <div className="absolute mt-2 bg-white shadow-lg rounded-md p-2 w-64 z-10">
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Erection & Commissioning</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">ISO Certification</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Welding & NDT Services</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Crane Hiring Services</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">NDT Services</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Manpower Outsourcing</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Special Tech Services</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Supply chain management</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Expediting & Inspection</p>
+                  <div className="absolute mt-2 bg-white shadow-lg rounded-md p-4 w-80 z-10">
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Erection & Commissioning</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">ISO Certification</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Welding & NDT Services</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Crane Hiring Services</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">NDT Services</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Manpower Outsourcing</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Special Tech Services</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Supply chain management</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Expediting & Inspection</p>
                   </div>
                 )}
               </div>
@@ -114,39 +114,39 @@ const Navbar = () => {
               <div className="relative">
                 <button 
                   onClick={() => setIsMoreOpen(!isMoreOpen)} 
-                  className="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  className="text-gray-500 hover:text-gray-700 inline-flex items-center px-2 pt-2 text-lg font-medium"
                 >
                   More {isMoreOpen ? (
-                    <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20" className="ml-1" xmlns="http://www.w3.org/2000/svg">
+                    <svg viewBox="0 0 24 24" fill="currentColor" width="24" height="24" className="ml-2" xmlns="http://www.w3.org/2000/svg">
                       <path d="M19 15l-7-7-7 7" />
                     </svg>
                   ) : (
-                    <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20" className="ml-1" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M19 9l-7 7-7-7" />
-                  </svg>
+                    <svg viewBox="0 0 24 24" fill="currentColor" width="24" height="24" className="ml-2" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 9l-7 7-7-7" />
+                    </svg>
                   )}
                 </button>
                 {isMoreOpen && (
-                  <div className="absolute mt-2 bg-white shadow-lg rounded-md p-2 w-64 z-10">
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Consulting Services</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Contract Management</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Project management</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Quality Management</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Materials Management</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Project Mgmt. Training</p>
-                    <p className="text-gray-700 text-sm py-2 px-4 hover:bg-gray-100 cursor-pointer">Documentation/ SOPs</p>
+                  <div className="absolute mt-2 bg-white shadow-lg rounded-md p-4 w-80 z-10">
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Consulting Services</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Contract Management</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Project management</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Quality Management</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Materials Management</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Project Mgmt. Training</p>
+                    <p className="text-gray-700 text-base py-2 px-4 hover:bg-gray-100 cursor-pointer">Documentation/ SOPs</p>
                   </div>
                 )}
               </div>
             </div>
-            <div className="flex items-center space-x-4">
+            <div className="flex items-center space-x-6">
               {/* User Icon */}
               <button className="text-gray-700 focus:outline-none">
                 <svg 
                   viewBox="0 0 24 24" 
                   fill="currentColor" 
-                  width="40px" 
-                  height="40px" 
+                  width="48" 
+                  height="48" 
                   className="text-gray-700" 
                   xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" d="M16.056 8.255a4.254 4.254 0 1 1-8.507 0 4.254 4.254 0 0 1 8.507 0zm3.052 11.71H4.496a.503.503 0 0 1-.46-.693 8.326 8.326 0 0 1 7.766-5.328 8.326 8.326 0 0 1 7.766 5.328.503.503 0 0 1-.46.694z"></path>
